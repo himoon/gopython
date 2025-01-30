24c08eab607fb03039722474106516af9cf486ac
hyun1moon <hyunil.moon@gmail.com>
2025-01-30 09:17:06 +0900

    Update comments for FSS_KEY

--- ch_10/step_1_2.py
+++ ch_10/step_1_2.py
@@ -9,7 +9,7 @@ OUT_1_2 = OUT_DIR / f"{Path(__file__).stem}.xlsx"
 
 
 def deposit_info_to_xlsx():
-    FSS_KEY = "금융감독원 API 인증키"  # 정기예금 API 인증키 입력
+    FSS_KEY = "금융감독원 API 인증키"  # 금융감독원 정기예금 API 인증키 입력
     fss = Fss(FSS_KEY)  # Fss 객체 생성
     resp = fss.deposit_search(fin_grp="은행", intr_rate_type="단리", save_trm="12", join_member="제한없음")  # API 호출
     df_raw = pd.DataFrame(resp)  # 데이터프레임 생성
--- ch_10/step_1_2_deposit.py
+++ ch_10/step_1_2_deposit.py
@@ -1,6 +1,6 @@
 from datakart import Fss
 
-FSS_KEY = "금융감독원 API 인증키"  # 정기예금 API 인증키 입력
+FSS_KEY = "금융감독원 API 인증키"  # 금융감독원 정기예금 API 인증키 입력
 fss = Fss(FSS_KEY)  # Fss 객체 생성
 resp = fss.deposit_search()  # 정기예금 API 조회
 item = resp.pop(0)  # 첫 번째 데이터 추출
