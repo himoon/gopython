06bb20d8f5b81721aa692fbcc34a90483efa5084
hyun1moon <hyunil.moon@gmail.com>
2025-02-14 00:30:33 +0900

    Add sample codes for TTS

--- ch_14/step_2_1.py
+++ ch_14/step_2_1.py
@@ -8,3 +8,8 @@ def tts_client() -> texttospeech.TextToSpeechClient:
     path = IN_DIR / "API_KEY.json"  # 서비스 계정 키 파일 경로 입력
     cred = service_account.Credentials.from_service_account_file(path)  # 자격 증명 객체
     return texttospeech.TextToSpeechClient(credentials=cred)  # 클라이언트 객체 반환
+
+
+if __name__ == "__main__":
+    client = tts_client()  # 클라이언트 객체 생성
+    print(client.list_voices())  # TTS 지원 언어 목록 출력
